b04ff1736b17d5702a27452f47491a6038f16ac8
Pranathi99 <pranathi.ganti@gmail.com>
2023-03-06 19:18:45 +0530

    Remove First Occurrence from LL, Stack

---
+++ JavaPracticeProjects/src/DSAlgo/DSALgo2.java
@@ -0,0 +1,29 @@
+package DSAlgo;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class DSALgo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*REMOVE FIRST OCCURRENCE FROM LINKEDLIST*/
+		LinkedList<Integer>ll=new LinkedList<Integer>(); 
+		for(int i=1;i<=10;i++)
+			ll.add(i);
+		System.out.println(ll);
+		int key=5;
+		ll.removeFirstOccurrence(key);
+		System.out.println(ll);
+		
+		Stack<Integer>st=new Stack<Integer>();
+		for(int i=1;i<=10;i++)
+			st.push(i);
+		System.out.println(st);
+		System.out.println(st.pop());
+		System.out.println(st.push(11));
+		System.out.println(st);
+	}
+
+}
