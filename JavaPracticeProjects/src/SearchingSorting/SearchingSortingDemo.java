341a0a99475c1a9d31adbb4d224bb91b1359be84
Pranathi99 <pranathi.ganti@gmail.com>
2023-03-06 21:04:26 +0530

    Binary Search

--- JavaPracticeProjects/src/SearchingSorting/SearchingSortingDemo.java
+++ JavaPracticeProjects/src/SearchingSorting/SearchingSortingDemo.java
@@ -4,6 +4,30 @@ public class SearchingSortingDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int n = LinearSearch();
+		System.out.println("index->"+n);
+		int x=BinarySearch();
+		System.out.println("index->"+x);
+	}
+
+	private static int BinarySearch() {
+		int arr[]= {1,3,5,6,7,9};
+		int n=arr.length;
+		int l=0,h=n-1,key=5;
+		while(l<=h)
+		{
+			int mid=l+(h-l)/2;
+			if(arr[mid]==key)
+				return mid;
+			else if(arr[mid]>key)
+				h=mid-1;
+			else
+				l=mid+1;
+		}
+		return -1;
+	}
+
+	private static int LinearSearch() {
 		int arr[]= {1,3,5,6,7,9};
 		int key=5,n=arr.length;
 		for(int i=0;i<n;i++)
@@ -11,10 +35,11 @@ public class SearchingSortingDemo {
 			if(key==arr[i])
 			{
 				System.out.println(i);
-				return;
+				return i;
 			}
 		}
 		System.out.println(-1);
+		return -1;
 	}
 
 }
