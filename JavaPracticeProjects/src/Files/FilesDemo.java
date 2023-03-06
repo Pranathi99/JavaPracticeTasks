b3a700007dbddd86a9d7568a494d7de09c139fcf
Pranathi99 <pranathi.ganti@gmail.com>
2023-03-06 18:04:36 +0530

    Read, write with FileReader and FileWriter

--- JavaPracticeProjects/src/Files/FilesDemo.java
+++ JavaPracticeProjects/src/Files/FilesDemo.java
@@ -14,6 +14,8 @@ public class FilesDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		FileCreation();
+		FileWriterDemo();
+		FileReaderDemo();
 	}
 
 	private static void FileCreation() {
@@ -23,6 +25,46 @@ public class FilesDemo {
 				System.out.println("File Created!");
 			else
 				System.out.println("File already exists!");
+		}
+		catch(IOException ex)
+		{
+			System.out.println(ex);
+		}
+	}
+	
+	private static void FileWriterDemo() {
+		// TODO Auto-generated method stub
+		int i;
+		try {
+			File f=new File("sample.txt");
+			FileWriter fw=new FileWriter(f);
+			fw.append("Hello World!");
+			fw.close();
+		}
+		catch(FileNotFoundException ex)
+		{
+			System.out.println(ex);
+		}
+		catch(IOException ex)
+		{
+			System.out.println(ex);
+		}
+	}
+
+	private static void FileReaderDemo() {
+		int i;
+		try {
+			File f=new File("sample.txt");
+			FileReader fr=new FileReader(f);
+			while((i=fr.read())!=-1)
+			{
+				System.out.print((char)i);
+			}
+			fr.close();
+		}
+		catch(FileNotFoundException ex)
+		{
+			System.out.println(ex);
 		}
 		catch(IOException ex)
 		{
