f6b5c8de50bb8a5cf76c4a62e7a0bfc5df982fb5
Pranathi99 <pranathi.ganti@gmail.com>
2023-03-06 18:51:58 +0530

    4th smallest element in unsorted array

--- JavaPracticeProjects/src/DSAlgo/DSAlgosDemo.java
+++ JavaPracticeProjects/src/DSAlgo/DSAlgosDemo.java
@@ -3,16 +3,26 @@ package DSAlgo;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class DSAlgosDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		/*ROTATE ARRAY RIGHT BY 5 TIMES*/
 		int [] arr= {1,2,3,4,5,6,7,8,9,10};
 		int i,n=arr.length,k=5;
 		rightRotate(arr,0,n,k);
 		for(int x:arr)
 			System.out.print(x+" ");
+		/*FOURTH SMALLEST ELEMENT IN UNSORTED ARRAY*/
+		int arr1[]= {2,4,1,46,32,86,-29};
+		PriorityQueue<Integer>pq=new PriorityQueue<Integer>();
+		for(int x:arr1)
+			pq.add(x);
+		for(i=0;i<3;i++)
+			pq.poll();
+		System.out.println(pq.peek());
 	}
 
 	private static void rightRotate(int[] arr, int i, int n, int k) {
