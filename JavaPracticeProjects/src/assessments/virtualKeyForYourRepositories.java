0539ed44a16867a56f904e35715e0f29fff2ca4e
Pranathi99 <pranathi.ganti@gmail.com>
2023-03-09 17:07:15 +0530

    Add/Search/Delete file (option 2)

--- JavaPracticeProjects/src/assessments/virtualKeyForYourRepositories.java
+++ JavaPracticeProjects/src/assessments/virtualKeyForYourRepositories.java
@@ -35,6 +35,42 @@ public class virtualKeyForYourRepositories {
 				getListOfFiles(mp.getOrDefault("~/",new ArrayList<String>()));
 				System.out.println("--------------------------------------------------");
 				break;
+			case "2":
+				while(true)
+				{
+					System.out.println("Choose an option:");
+					System.out.println(" 2a.Add a file");
+					System.out.println(" 2b.Delete a file");
+					System.out.println(" 2c.Search a file");
+					System.out.println(" 2d.Return to main content");
+					
+					String opt=sc.next();
+					if(opt.equals("2d"))
+						break;
+					
+					switch(opt) {
+					case "2a":
+						String fileName=sc.next();
+						addFile(fileName,mp);
+						System.out.println("--------------------------------------------------");
+					break;
+					case "2b":
+						fileName=sc.next();
+						deleteFile(fileName,mp);
+						System.out.println("--------------------------------------------------");
+						break;
+					case "2c":
+						fileName=sc.next();
+						searchFile(fileName,mp);
+						System.out.println("--------------------------------------------------");
+						break;
+					default:
+						System.out.println("Invalid option. Please try again!");
+						System.out.println("--------------------------------------------------");
+					}
+				}
+				System.out.println("--------------------------------------------------");
+				break;
 			
 			default:
 					System.out.println("Please enter valid input operation");	
@@ -44,7 +80,73 @@ public class virtualKeyForYourRepositories {
 		
 	}
 
-	
+	private static void searchFile(String fileName, Map<String, List<String>> mp) {
+		Iterator it=mp.entrySet().iterator();
+		List<String>filesPathList=new ArrayList<String>();
+		while(it.hasNext()) {
+            Map.Entry entry = (Map.Entry)it.next();
+            String key=(String) entry.getKey();
+ 
+            // Adding some bonus marks to all the students
+            List<String>list=(List<String>) entry.getValue();
+            for(String file:list)
+            {
+            	if(file.equals(fileName))
+            	{
+            		filesPathList.add(key+fileName);
+            	}
+            }
+        }
+		if(filesPathList.size()>0)
+		{
+			System.out.println("FileName found at locations:");
+			for(String s:filesPathList)
+				System.out.println(s);
+		}
+		else
+		{
+			System.out.println("File not found!");
+		}
+    }
+
+	private static void deleteFile(String fileName, Map<String, List<String>> mp) {
+		String pathName=fileName.substring(0, fileName.lastIndexOf("/")+1);
+		System.out.println(pathName);
+		fileName=fileName.substring(fileName.lastIndexOf("/")+1);
+		if(pathName.equals(""))
+			pathName="~/";
+		List<String>al=mp.getOrDefault(pathName,new ArrayList<String>());
+		if(al.contains(fileName))
+		{
+			al.remove(fileName);
+			mp.put(pathName, al);
+		}
+		else
+		{
+			System.out.println("File does not exist in given directory to delete");
+		}
+	}
+
+	private static void addFile(String fileName, Map<String, List<String>> mp) {
+		String pathName=fileName.substring(0, fileName.lastIndexOf("/")+1);
+		System.out.println(pathName);
+		fileName=fileName.substring(fileName.lastIndexOf("/")+1);
+		if(fileName.indexOf(".")==-1)
+		{
+			System.out.println("Invalid FileName. Please try again!");
+			return;
+		}
+		if(pathName.equals(""))
+			pathName="~/";
+		List<String>al=mp.getOrDefault(pathName,new ArrayList<String>());
+		if(al.contains(fileName))
+			System.out.println("File already exists");
+		else
+		{
+			al.add(fileName);
+			mp.put(pathName, al);
+		}
+	}
 
 	private static void getListOfFiles(List<String> list) {
 		if(list.size()==0)
